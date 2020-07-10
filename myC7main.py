78dbd165787e171cefdc98a17c13517e29c2249a
epm1989 <epm1989@yahoo.com>
2020-07-10 09:43:41 -0500

    se inserta el snipet de codigo de la CLI interactiva y la estructura __name__ basica

---
+++ myC7main.py
@@ -0,0 +1,36 @@
+from time import sleep;
+
+def run():
+    while True:
+        saludo='''
+        ##############################################
+        ######Bienvenido a la prueba de Csiete.#######
+        ##############################################
+        '''
+        print(saludo)
+        
+        opciones='''
+        -->>>>>>>
+        1. Listar Dominios.
+        2. Agregar Dominios.
+        3. Borrar Dominio.
+        4. Abortar.
+        -->>>>>>>
+        :'''
+        try:
+            choice=input(opciones)
+            choice=int(choice)
+            if choice > 4 or choice == 0:
+                raise ValueError('Opcion fuera del rango.')
+            if choice == 4 :
+                break
+        except ValueError as identifier:
+            print(identifier)
+            print('Debe de ingresar un Numero. no un texto')
+        finally:
+            sleep(2)
+
+
+
+if __name__ == "__main__":
+    run()
